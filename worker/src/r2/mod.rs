42c921b465746f3d8d2ec04a95f3a08139aef8cf
Felix <me@mbilker.us>
2024-03-06 09:03:15 +0000

    fix: expose R2 multipart upload key getter

--- worker/src/r2/mod.rs
+++ worker/src/r2/mod.rs
@@ -358,6 +358,10 @@ pub struct MultipartUpload {
 }
 
 impl MultipartUpload {
+    pub fn key(&self) -> String {
+        self.inner.key().unwrap()
+    }
+
     /// Uploads a single part with the specified part number to this multipart upload.
     ///
     /// Returns an [UploadedPart] object containing the etag and part number.
